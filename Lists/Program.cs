ccb6ab8d5cd547c9ce082df2918d548c855feec3
Clark Miller <glitchclick@gmail.com>
2021-03-18 15:03:41 -0400

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Arthur Dent", "Marvin" };
+
+            names.Add("Trillian");
+            names.Add("Ford Prefect");
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[names.Count - 1]);
+            names[0] = "Arthur";
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[2]);
+            Console.ReadLine();
+        }
+    }
+}
