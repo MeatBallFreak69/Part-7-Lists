29557c17d07a839f060aadb17fd745e54c52ee32
Clark Miller <glitchclick@gmail.com>
2021-03-18 17:37:02 -0400

    Made entire program

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -10,15 +10,59 @@ namespace Lists
     {
         static void Main(string[] args)
         {
+            string upperReadLine;
             List<string> names = new List<string>() { "Arthur Dent", "Marvin" };
-
             names.Add("Trillian");
             names.Add("Ford Prefect");
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[names.Count - 1]);
-            names[0] = "Arthur";
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[2]);
+            for (int i = 0; i < names.Count; i++)
+                names[i] = names[i].ToUpper();
+
+            for (int p = 0; p < names.Count; p++)
+                Console.WriteLine(names[p]);
+
+            Console.WriteLine("");
+
+            names.Sort();
+
+            foreach (string name in names) 
+                Console.WriteLine(name);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Please add 3 new names");
+            for (int i = 0; i < 3; i++)
+                names.Add(Console.ReadLine().ToUpper());
+
+            Console.WriteLine("");
+
+            names.Sort();
+
+            foreach (string name in names)
+                Console.WriteLine(name);
+            Console.WriteLine($"In this list there are {names.Count} names");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Please enter a name to remove it from the list");
+            upperReadLine = Console.ReadLine().ToUpper();
+            if (names.Remove(upperReadLine))
+                Console.WriteLine($"{upperReadLine} deleted");
+            else
+            {
+                names.Add(upperReadLine);
+                Console.WriteLine($"{upperReadLine} added");
+            }
+
+            Console.WriteLine("");
+
+            foreach (string name in names)
+                Console.WriteLine(name);
+            Console.WriteLine($"In this list there are {names.Count} names");
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Press enter to quit");
             Console.ReadLine(); 
         }
     }
